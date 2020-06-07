68f6bd96170fe35e231bf1df12784d50e4a06b46
Jamezjaz <jamezjaz@gmail.com>
2020-06-07 16:13:35 -0500

    Update  main.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -5,4 +5,8 @@ require_relative '../lib/inspire.rb'
 puts 'Hello!,   Welcome to Inspirational Telegram bot'
 puts 'This bot inspires and entertains you by giving you some inspirational quotes'
 
+puts '----------------------------------------------'
+puts 'Now, navigate to your telegram account, search for ComicBot and punch /start to start'
+puts '...loading the bot is now online... ctrl + c to stop the bot'
+
 Bot.new

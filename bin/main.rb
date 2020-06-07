cfa9d7fda484cea1175aa582e7e2f0dfb2dba79e
Jamezjaz <jamezjaz@gmail.com>
2020-06-07 03:40:07 -0500

    Add welcome messages on main.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,6 @@
 require_relative '../lib/bot.rb'
 require_relative '../lib/inspire.rb'
+
+#All puts
+puts 'Hello!,   Welcome to Inspirational Telegram bot'
+puts 'This bot inspires and entertains you by giving you some inspirational quotes'

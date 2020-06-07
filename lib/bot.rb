7512f658c2fcdee9f14a7e9a8b1a1b28f439132c
Jamezjaz <jamezjaz@gmail.com>
2020-06-07 03:22:16 -0500

    Add bot.rb and inspire.rb files

---
+++ lib/bot.rb
@@ -0,0 +1,14 @@
+require 'telegram/bot'
+
+=begin
+LOGIC
+Create a Bot class that will host the telegram bot API
+Create an Inspire class in the inspire.rb file, it will make a request to an API endpoint
+Use bot.api to send a message to the chat_id; chat is the target id or the username of the user
+=end
+
+class Bot
+    def initialize
+        token = '1180933661:AAFuUcHVE8NacW2V7fuOCA7cAAI_0n_j6ug'
+    end
+end

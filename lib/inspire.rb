60a1508a754b0b877dba00f80707c6ed90e21c6e
Jamezjaz <jamezjaz@gmail.com>
2020-06-07 05:04:31 -0500

    Add quotes_request method that returns quotes

--- lib/inspire.rb
+++ lib/inspire.rb
@@ -1,7 +1,21 @@
+require 'telegram/bot'
+require 'net/http'
+require 'json'
+require_relative 'bot.rb'
+
 class Inspire
     @quotes = nil
 
     def initialize
         @quotes = quotes_request
+    end
+
+    #Mkakes requests to an endpoint that returns motivational messages as JSON response
+    def quotes_request
+        url = 'https://programming-quotes-api.herokuapp.com/quotes/lang/en'
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        response = JSON. parse(response)
+        response
     end
 end

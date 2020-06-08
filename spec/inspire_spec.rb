8180d4128c11c0dcbcbd93de9a1b0e9bc46fa929
Jamezjaz <jamezjaz@gmail.com>
2020-06-08 15:35:56 -0500

    Add test for #select_randomly method

--- spec/inspire_spec.rb
+++ spec/inspire_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/inspire.rb'
 
 describe Inspire do
+    let(:value) { Inspire.new }
     describe '#message_request' do
-        let(:value) { Inspire.new }
         let(:json_request) { value.message_request }
 
         it 'returns a JSON response if request is successful' do
@@ -11,6 +11,18 @@ describe Inspire do
 
         it 'returns "should not be empty" if it is empty' do
             expect(json_request.length).not_to be_nil
+        end
+    end
+
+    describe '#select_randomly' do
+        let(:random) { value.select_randomly }
+
+        it 'returns a Hash' do
+            expect(random.class).to be(Hash)
+        end
+
+        it 'returns a key and a value' do
+            expect(random.length).not_to be(0)
         end
     end
 end

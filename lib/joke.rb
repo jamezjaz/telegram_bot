5a284df312b8ca78d5de802fb2dd778209d19d0c
Jamezjaz <jamezjaz@gmail.com>
2020-06-09 10:45:30 -0500

    Inherit the attributes of the Inspire class by the Joke class as per DRY principle

--- lib/joke.rb
+++ lib/joke.rb
@@ -2,14 +2,9 @@ require 'telegram/bot'
 require 'net/http'
 require 'json'
 require_relative 'bot.rb'
+require File.expand_path('inspire.rb', __dir__)
 
-class Joke
-  @quotes = nil
-
-  def initialize
-    @quotes = message_request
-  end
-
+class Joke < Inspire
   def message_request
     url = 'https://official-joke-api.appspot.com/random_joke'
     uri = URI(url)

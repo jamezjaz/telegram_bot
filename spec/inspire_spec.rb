99fe90124abd9a93cbb1a7572e5947d1db775273
Jamezjaz <jamezjaz@gmail.com>
2020-06-10 17:22:18 -0500

    Remove test for #message_request private method

--- spec/inspire_spec.rb
+++ spec/inspire_spec.rb
@@ -2,17 +2,6 @@ require_relative '../lib/inspire.rb'
 
 describe Inspire do
   let(:value) { Inspire.new }
-  describe '#message_request' do
-    let(:json_request) { value.message_request }
-
-    it 'returns a JSON response if request is successful' do
-      expect(json_request.class).to eql(Array)
-    end
-
-    it 'returns "should not be empty" if it is empty' do
-      expect(json_request.length).not_to be_nil
-    end
-  end
 
   describe '#select_randomly' do
     let(:random) { value.select_randomly }
